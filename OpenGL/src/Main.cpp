4a8976cf0d3271103115ff955982668acda43959
Joshua Moellers <joshua.moellers@gmx.net>
2018-03-24 11:27:39 +0100

    Abstracted Shader

--- OpenGL/src/Main.cpp
+++ OpenGL/src/Main.cpp
@@ -12,103 +12,13 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
+#include "Shader.h"
 
-struct ShaderSource
-{
-	std::string VertexSource;
-	std::string FragmentSource;
-};
 
 enum class rgba
 {
 	RED = 0, GREEN = 1, BLUE = 2, ALPHA = 3
 };
-
-static ShaderSource GetShaderSource(const std::string& filepath) 
-{
-	std::ifstream stream(filepath);
-	std::stringstream ss[2];
-	std::string line;
-	enum class ShaderType
-	{
-		NONE = -1, VERTEX = 0, FRAGMENT = 1
-	};
-
-	ShaderType type = ShaderType::NONE;
-
-	while (getline(stream, line)) 
-	{
-		if (line.find("#shader") != std::string::npos) 
-		{
-			if (line.find("vertex") != std::string::npos)
-			{
-				type = ShaderType::VERTEX;
-			}
-			else if (line.find("fragment") != std::string::npos)
-			{
-				type = ShaderType::FRAGMENT;
-			}
-			continue;
-		}
-
-		if (type == ShaderType::NONE)
-			continue;
-		ss[(int)type] << line << "\n";
-	}
-	return { ss[0].str(), ss[1].str() };
-}
-
-ShaderSource shaderSource = GetShaderSource("res/shader/Basic.shader");
-
-static unsigned int CompileShader(unsigned int type, const std::string& source) 
-{
-	GLAssertError(unsigned int id = glCreateShader(type));
-	const char* src = source.c_str();
-	GLAssertError(glShaderSource(id, 1, &src, nullptr));
-	GLAssertError(glCompileShader(id));
-	
-	int result;
-	GLAssertError(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
-	if (result == GL_FALSE) {
-		char infoLog[512];
-		GLAssertError(glGetShaderInfoLog(id, 512, NULL, infoLog));
-		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
-		std::cout << infoLog << std::endl;
-		GLAssertError(glDeleteShader(id));
-		return 0;
-	}
-	
-	return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) 
-{
-	GLAssertError(unsigned int program = glCreateProgram());
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-	GLAssertError(glAttachShader(program, vs));
-	GLAssertError(glAttachShader(program, fs));
-	GLAssertError(glLinkProgram(program));
-	
-	int result;
-	GLAssertError(glGetProgramiv(program, GL_LINK_STATUS, &result));
-	if (result == GL_FALSE)
-	{
-		char infoLog[512];
-		GLAssertError(glGetProgramInfoLog(program, 512, NULL, infoLog));
-		std::cout << "Failed to Link Shader Program" << std::endl;
-		std::cout << infoLog << std::endl;
-		GLAssertError(glDeleteProgram(program));
-		GLAssertError(glDeleteShader(vs));
-		GLAssertError(glDeleteShader(fs));
-		return 0;
-	}
-
-	GLAssertError(glDeleteShader(vs));
-	GLAssertError(glDeleteShader(fs));
-
-	return program;
-}
 
 int main(void)
 {
@@ -166,22 +76,29 @@ int main(void)
 		vao.AddBuffer(vbo, layout);
 		IndexBuffer ibo(indices, 6);
 
-		unsigned int shader = CreateShader(shaderSource.VertexSource, shaderSource.FragmentSource);
-		GLAssertError(glUseProgram(shader));
+		Shader shader("res/shaders/Basic.shader");
+		shader.Bind();
 
-		GLAssertError(int u_ColorId = glGetUniformLocation(shader, "u_Color"));
-		AssertBreak(u_ColorId == -1);
-		GLAssertError(glUniform4f(u_ColorId, color[(int)rgba::RED], color[(int)rgba::GREEN], color[(int)rgba::BLUE], color[(int)rgba::ALPHA]));
+		shader.SetUniform4f("u_Color", color[(int)rgba::RED], color[(int)rgba::GREEN], color[(int)rgba::BLUE], color[(int)rgba::ALPHA]);
 
 		float rInc = 0.03f;
 		float gInc = 0.05f;
 		float bInc = 0.07f;
+
+		vao.Unbind();
+		vbo.Unbind();
+		ibo.Unbind();
+		shader.Unbind();
+
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			vao.Bind();
+			shader.Bind();
 
 			if (color[(int)rgba::RED] >= 1 || color[(int)rgba::RED] <= 0)
 				rInc = -rInc;
@@ -194,7 +111,7 @@ int main(void)
 			color[(int)rgba::GREEN] += gInc;
 			color[(int)rgba::BLUE] += bInc;
 
-			GLAssertError(glUniform4f(u_ColorId, color[(int)rgba::RED], color[(int)rgba::GREEN], color[(int)rgba::BLUE], color[(int)rgba::ALPHA]));
+			shader.SetUniform4f("u_Color", color[(int)rgba::RED], color[(int)rgba::GREEN], color[(int)rgba::BLUE], color[(int)rgba::ALPHA]);
 
 			GLAssertError(glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, nullptr));
 
@@ -204,8 +121,6 @@ int main(void)
 			/* Poll for and process events */
 			glfwPollEvents();
 		}
-
-		GLAssertError(glDeleteProgram(shader));
 	}
 	glfwTerminate();
 	
---
+++ OpenGL/src/Shader.cpp
@@ -0,0 +1,131 @@
+#include "Shader.h"
+#include "Renderer.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+Shader::Shader(const std::string & shaderFile)
+	:m_RendererID(0), m_Filepath(shaderFile)
+{
+	ShaderSource source = ParseShader(shaderFile);
+	m_RendererID = CreateShader(source.VertexSource, source.FragmentSource);
+}
+
+Shader::~Shader()
+{
+	GLAssertError(glDeleteProgram(m_RendererID));
+}
+
+void Shader::Bind() const
+{
+	GLAssertError(glUseProgram(m_RendererID));
+}
+
+void Shader::Unbind() const
+{
+	GLAssertError(glUseProgram(0));
+}
+
+unsigned int Shader::GetUniformLocation(const std::string& name)
+{
+	if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
+		return m_UniformLocationCache[name];
+
+	GLAssertError(int location = glGetUniformLocation(m_RendererID, name.c_str()));
+	if (location == -1)
+	{
+		std::cout << "Warning! Uniform" << name << "doesn´t exist!" << std::endl;
+	}
+	m_UniformLocationCache[name] = location;
+	return location;
+}
+
+void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
+{
+	GLAssertError(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
+}
+
+ShaderSource Shader::ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+	std::stringstream ss[2];
+	std::string line;
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+			continue;
+		}
+
+		if (type == ShaderType::NONE)
+			continue;
+		ss[(int)type] << line << "\n";
+	}
+	return { ss[0].str(), ss[1].str() };
+}
+
+unsigned int Shader::CompileShader(unsigned int type, const std::string& source)
+{
+	GLAssertError(unsigned int id = glCreateShader(type));
+	const char* src = source.c_str();
+	GLAssertError(glShaderSource(id, 1, &src, nullptr));
+	GLAssertError(glCompileShader(id));
+
+	int result;
+	GLAssertError(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
+	if (result == GL_FALSE) {
+		char infoLog[512];
+		GLAssertError(glGetShaderInfoLog(id, 512, NULL, infoLog));
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << infoLog << std::endl;
+		GLAssertError(glDeleteShader(id));
+		return 0;
+	}
+
+	return id;
+}
+
+unsigned int Shader::CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	GLAssertError(unsigned int program = glCreateProgram());
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+	GLAssertError(glAttachShader(program, vs));
+	GLAssertError(glAttachShader(program, fs));
+	GLAssertError(glLinkProgram(program));
+
+	int result;
+	GLAssertError(glGetProgramiv(program, GL_LINK_STATUS, &result));
+	if (result == GL_FALSE)
+	{
+		char infoLog[512];
+		GLAssertError(glGetProgramInfoLog(program, 512, NULL, infoLog));
+		std::cout << "Failed to Link Shader Program" << std::endl;
+		std::cout << infoLog << std::endl;
+		GLAssertError(glDeleteProgram(program));
+		GLAssertError(glDeleteShader(vs));
+		GLAssertError(glDeleteShader(fs));
+		return 0;
+	}
+
+	GLAssertError(glDeleteShader(vs));
+	GLAssertError(glDeleteShader(fs));
+
+	return program;
+}
